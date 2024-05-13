c98eda10a7c8090d7fbb021d2fe3701dca07c615
alexismanuel <alexis.manuelpro@gmail.com>
2024-05-13 20:46:09 +0200

    feat(cli): basic cli to run stack

--- fastack.go
+++ fastack.go
@@ -3,15 +3,71 @@ package main
 import (
 	"context"
 	"fastack/docker"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
 )
 
-func main() {
+var stackFolder = "./stacks"
+
+type StackConfig struct {
+	Name   string `yaml:"name"`
+	Mode   string `yaml:"mode"`
+	Config struct {
+		ImageName   string `yaml:"image_name"`
+		ExposedPort string `yaml:"container_port"`
+		HostPort    string `yaml:"host_port"`
+		AppName     string `yaml:"app_name"`
+	} `yaml:"config"`
+}
+
+func RunDocker(stackConfig StackConfig) {
 	ctx := context.Background()
 	d := docker.New(ctx)
+	msg := fmt.Sprintf("Now runnning %s", stackConfig.Name)
+	fmt.Println(msg)
 	d.CreateAndRun(
-		"mongo",
-		"8080",
-		"8080",
-		"mongo-go-cli",
+		stackConfig.Config.ImageName,
+		stackConfig.Config.ExposedPort,
+		stackConfig.Config.HostPort,
+		stackConfig.Config.AppName,
 	)
+}
+
+func main() {
+	stackName := flag.String("stack", "", "Stack name to run")
+	flag.Parse()
+
+	if *stackName == "" {
+		fmt.Println("Please provide a stack to run.")
+		flag.Usage()
+		return
+	}
+
+	fileLocation := fmt.Sprintf("%s/%s.yml", stackFolder, *stackName)
+	filename, _ := filepath.Abs(fileLocation)
+	fmt.Println(filename)
+	yamlFile, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	var stackConfig StackConfig
+	err = yaml.Unmarshal(yamlFile, &stackConfig)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(stackConfig)
+
+	switch stackConfig.Mode {
+	case "docker":
+		RunDocker(stackConfig)
+	default:
+		msg := fmt.Sprintf("Unknown mode found: %s", stackConfig.Mode)
+		fmt.Println(msg)
+	}
+
 }
